0cc1426fe7a6100a820c8c5c1dad6489fbdc48de
Patrick Musau <pmusau13ster@gmail.com>
2016-02-11 11:16:14 -0600

    fused images complete

---
+++ src/computer_vision/nodes/ros_classifier_fused.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image,LaserScan
+from message_filters import ApproximateTimeSynchronizer, Subscriber
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np 
+import imutils
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
+
+# import the custom message files defined the race package
+from racecar.msg import drive_param
+from racecar.msg import angle_msg
+
+#import the tensorflow package
+from tensorflow.python.keras.models import load_model
+from tensorflow.python.keras.backend import clear_session
+
+# import sys so we can use packages outside of this folder in
+# either python 2 or python 3, I know it's janky, chill
+import sys
+import os
+from pathlib import Path 
+#insert parent directory into the path
+sys.path.insert(0,str(Path(os.path.abspath(__file__)).parent.parent))
+print(sys.path)
+
+#import the preprocessing utils (helps with loading data, preprocessing)
+from preprocessing.utils import ImageUtils
+
+clear_session()
+
+class ROS_Classify:
+
+    #define the constructor 
+    def __init__(self,model,decoupled=False,plot=False):
+
+
+        self.cv_bridge=CvBridge()
+
+        self.image_rect_color_left=Subscriber('/zed/zed_node/left/image_rect_color',Image)
+        self.image_rect_color_right=Subscriber('/zed/zed_node/right/image_rect_color',Image)
+        self.lidar_sub=Subscriber('scan',LaserScan)
+
+        #create the time synchronizer
+        self.sub = ApproximateTimeSynchronizer([self.image_rect_color_left,self.image_rect_color_right,self.lidar_sub], queue_size = 50, slop = 0.05)
+
+        # load the model 
+        self.model=load_model(model)
+
+        # keras predict function 
+        self.model._make_predict_function()
+
+        self.count = 0
+        self.util=ImageUtils()
+
+
+        #register the callback to the synchronizer
+        self.sub.registerCallback(self.image_callback)
+
+        # toggle plotting 
+        self.plot = plot
+        #depends how the model was trained
+        try:
+            self.height=self.model.layers[0].input_shape[1]
+            self.width=self.model.layers[0].input_shape[2]
+        except IndexError as e:
+            self.height=self.model.layers[0].input_shape[0][1]
+            self.width=self.model.layers[0].input_shape[0][2]
+        
+        if(not self.height==32 or not self.width ==32):
+            print("Model provided is incompatible, the image size should be 32 x 32 it is",self.height,"x",self.width)
+            exit()
+
+        self.classes=['left','right','straight','weak_left','weak_right']
+
+        self.decoupled=decoupled
+        if (not self.decoupled):
+            self.pub=rospy.Publisher('drive_parameters', drive_param, queue_size=5)
+        else:
+            self.pub=rospy.Publisher('angle_msg',angle_msg,queue_size=5)
+        
+
+        if self.plot:
+            #fields for plotting
+            self.commands=[]
+            self.times=[]
+            self.start_time=time.time()
+            #figure for live animation
+            self.fig = plt.figure()
+            self.ax = self.fig.add_subplot(1, 1, 1)
+            self.window=4000
+            #Animation
+            # Set up plot to call animate() function periodically
+            ani = animation.FuncAnimation(self.fig, self.animate, fargs=(self.commands, self.times), interval=1000)
+            plt.show()
+        
+
+    #image callback
+    def image_callback(self,image_left,image_right,lidar_msg):
+        #convert the ros_image to an openCV image
+        try:
+            orig_image=self.cv_bridge.imgmsg_to_cv2(image_left,"bgr8")/255.0
+            cv_image=self.util.reshape_image(orig_image,32,32)
+            cv_image2=self.cv_bridge.imgmsg_to_cv2(image_right,"bgr8")/255.0
+            cv_image2=self.util.reshape_image(cv_image2,32,32)
+            #print(cv_image.shape)
+        except CvBridgeError as e:
+            print(e)
+
+
+        limited_ranges=np.asarray(lidar_msg.ranges)
+        indices=np.where(limited_ranges>=10.0)[0]
+        limited_ranges[indices]=10.0
+        limited_ranges= limited_ranges[29:1053]
+        limited_ranges = limited_ranges.reshape((32,32,1))
+        limited_ranges = limited_ranges
+
+        cv_image= np.concatenate((cv_image,limited_ranges),axis = -1)
+        cv_image2= np.concatenate((cv_image2,limited_ranges),axis = -1)
+        imgs = np.asarray([cv_image,cv_image2])
+
+    
+        pred=self.model.predict(imgs)
+        self.count+=1
+
+        pred1 = pred[0].argmax()
+        pred2 = pred[1].argmax()
+
+        pred = np.mean(pred,axis=0)
+
+        print(self.classes[pred1],self.classes[pred2],self.classes[pred.argmax()])
+
+        #publish the actuation command
+        if(self.count>20):
+            self.send_actuation_command(pred)
+
+
+    #computes the actuation command to send to the car
+    def send_actuation_command(self,pred):
+        #get the label
+        label=self.classes[pred.argmax()]
+        if (label=="left"):
+            angle=0.5108652353
+        elif (label=="right"):
+            angle=-0.5108652353
+        elif (label=="straight"):
+            angle=0.0
+        elif (label=="weak_left"):
+            angle=0.10179
+        elif (label=="weak_right"):
+            angle=-0.10179
+        else: 
+            print("error:",label)
+            angle=0
+
+        if (self.plot):
+            self.commands.append(angle)
+            self.times.append(time.time()-self.start_time)
+
+        if (not self.decoupled):
+            msg = drive_param()
+            msg.header.stamp=rospy.Time.now()
+            msg.angle = angle
+            msg.velocity = 0.4
+        else:
+            msg=angle_msg()
+            msg.header.stamp=rospy.Time.now()
+            msg.steering_angle=angle
+        self.pub.publish(msg)
+
+    #function that animates the plotting
+    def animate(self,i,commands,times):
+        # Limit x and y lists to window items
+        self.commands = self.commands[-self.window:]
+        self.times = self.times[-self.window:]
+        # Draw x and y lists
+        self.ax.clear()
+        self.ax.plot(self.times, self.commands)
+
+        # Format plot
+        plt.xticks(rotation=45, ha='right')
+        plt.subplots_adjust(bottom=0.30)
+        plt.title('Time (s) vs Steering Angle (radians)')
+        plt.ylabel('Steering Angle (radians)')
+        plt.xlabel('Time (s)')
+
+
+if __name__=='__main__':
+    rospy.init_node("classify_node",anonymous=True)
+    #get the arguments passed from the launch file
+    args = rospy.myargv()[1:]
+    #get the racecar name so we know what to subscribe to
+    #get the keras model
+    model=args[0]
+
+
+    #if there's more than two arguments then its decoupled
+    if len(args)>2:
+        il=ROS_Classify(model,decoupled=True)
+    else:
+        il=ROS_Classify(model)
+    try: 
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting Down")
+        cv2.destroyAllWindows()
--- src/computer_vision/nodes/synchronize_img_command_lidar.py
+++ src/computer_vision/nodes/synchronize_img_command_lidar.py
@@ -2,7 +2,7 @@
 import rospy
 import cv2
 from std_msgs.msg import String
-from sensor_msgs.msg import Image, CompressedImage
+from sensor_msgs.msg import Image, CompressedImage, LaserScan
 from cv_bridge import CvBridge, CvBridgeError
 from message_filters import ApproximateTimeSynchronizer, Subscriber
 from ackermann_msgs.msg import AckermannDriveStamped
@@ -10,6 +10,17 @@ import imutils
 from racecar.msg import drive_param
 import os
 import rospkg
+
+import numpy as np
+
+# import sys so we can use packages outside of this folder in
+# either python 2 or python 3, I know it's janky, chill
+import sys
+import os
+from pathlib import Path 
+#insert parent directory into the path
+sys.path.insert(0,str(Path(os.path.abspath(__file__)).parent.parent))
+from preprocessing.utils import ImageUtils 
 
 
 class MessageSynchronizer:
@@ -22,21 +33,22 @@ class MessageSynchronizer:
         self.image_rect_color_left=Subscriber('/zed/zed_node/left/image_rect_color',Image)
         self.image_rect_color_right=Subscriber('/zed/zed_node/right/image_rect_color',Image)
         self.ackermann_stamped=Subscriber('/ackermann_cmd_mux/input/teleop',AckermannDriveStamped)
+        self.lidar_sub=Subscriber('/scan',LaserScan)
         r = rospkg.RosPack()
-        self.save_path_root=r.get_path('computer_vision')+'/data/'
+        self.save_path_root=r.get_path('computer_vision')+'/data2/'
         self.cv_bridge=CvBridge()
         self.count=0
         self.save_count=0
         self.straight_count =0 
 
         #create the time synchronizer
-        self.sub = ApproximateTimeSynchronizer([self.image_rect_color_left,self.image_rect_color_right,self.ackermann_stamped], queue_size = 20, slop = 0.05)
+        self.sub = ApproximateTimeSynchronizer([self.image_rect_color_left,self.image_rect_color_right,self.ackermann_stamped,self.lidar_sub], queue_size = 20, slop = 0.05)
         #register the callback to the synchronizer
         self.sub.registerCallback(self.master_callback)
 
     #callback for the synchronized messages
     #Note: a negative value means turning to the right, a postive value means turning to the left
-    def master_callback(self,image_left,image_right,ackermann_msg): #drive_param):
+    def master_callback(self,image_left,image_right,ackermann_msg,lidar_msg): #drive_param):
         #convert rosmsg to cv image
         try:
             cv_image=self.cv_bridge.imgmsg_to_cv2(image_left,"bgr8")
@@ -52,15 +64,28 @@ class MessageSynchronizer:
         command='%.10f' % ackermann_msg.drive.steering_angle
         #replace the period with ~ so it's a valid filename
         command=command.replace('.','~')
+
+        limited_ranges=np.asarray(lidar_msg.ranges)
+        indices=np.where(limited_ranges>=10.0)[0]
+        limited_ranges[indices]=10.0
+        limited_ranges= limited_ranges[29:1053]
+        limited_ranges = limited_ranges.reshape((32,32,1))
+        limited_ranges = limited_ranges
         
         #save path 
-        save_path=self.save_path_root+self.label_image(ackermann_msg.drive.steering_angle)+'/'+str(rospy.Time.now())+'~'+command+'.jpg'
-        save_path2=self.save_path_root+self.label_image(ackermann_msg.drive.steering_angle)+'/'+str(rospy.Time.now())+'~'+command+'.jpg'
+        save_path=self.save_path_root+self.label_image(ackermann_msg.drive.steering_angle)+'/'+str(rospy.Time.now())+'~'+command+'.png'
+        save_path2=self.save_path_root+self.label_image(ackermann_msg.drive.steering_angle)+'/'+str(rospy.Time.now())+'~'+command+'.png'
         print(self.label_image(ackermann_msg.drive.steering_angle))
 
-        if(self.count % 3==0 ackermann_msg.drive.speed>0.03):
-            self.save_image(cv_image,save_path)
-            self.save_image(cv_image2,save_path2)
+        if(self.count % 1==0 and ackermann_msg.drive.speed>0.03):
+            dirPath = os.path.split(save_path)[0]
+            if  True:
+                self.save_image(cv_image,save_path)
+                self.save_image(cv_image2,save_path2)
+                np.save(save_path.replace(".png",".npy"),limited_ranges)
+                np.save(save_path2.replace(".png",".npy"),limited_ranges)
+                self.save_count+=1
+        self.count+=1
         
     #function that categorizes images into left, weak_left, straight, weak_right, right
     def label_image(self,steering_angle):
@@ -78,19 +103,12 @@ class MessageSynchronizer:
     
     def save_image(self,image,path):
         dirPath = os.path.split(path)[0]
+        # if the output directory does not exist, create it
         if not os.path.exists(dirPath):
             os.makedirs(dirPath)
             print('does not exist')
-
-        if not "straight" in dirPath:
-            self.save_count+=1
-            cv2.imwrite(path,image)
-       	    print(self.save_count)
-        else: 
-            self.straight_count+=1
-            if(self.straight_count%5 == 0):
-                cv2.imwrite(path,image)
-       	        print(self.save_count)
+        print(path)
+        cv2.imwrite(path,image)
 
 
 if __name__=='__main__':
@@ -101,3 +119,4 @@ if __name__=='__main__':
     
     # spin so that we can receive messages
     rospy.spin()    
+
