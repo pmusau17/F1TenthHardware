0408928465e03872ed5c4d126b4a38cc29c19db4
Patrick Musau <pmusau13ster@gmail.com>
2020-10-09 16:44:34 -0500

    a whole bunch of trials in localization

---
+++ src/racecar/scripts/reset_odometry.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+import atexit
+import tf
+from os.path import expanduser
+from time import gmtime, strftime
+from numpy import linalg as LA
+from tf.transformations import euler_from_quaternion
+from nav_msgs.msg import Odometry
+from std_srvs.srv import Empty
+
+
+reset_odometry = rospy.ServiceProxy('/zed/zed_node/reset_odometry', Empty)
+reset_tracking = rospy.ServiceProxy('/zed/zed_node/reset_tracking', Empty)
+
+
+def subscribe_data(data):
+    quaternion = np.array([data.pose.pose.orientation.x, 
+                           data.pose.pose.orientation.y, 
+                           data.pose.pose.orientation.z, 
+                           data.pose.pose.orientation.w])
+
+    euler = tf.transformations.euler_from_quaternion(quaternion)
+    speed = LA.norm(np.array([data.twist.twist.linear.x, 
+                              data.twist.twist.linear.y, 
+                              data.twist.twist.linear.z]),2)
+    
+    x = data.pose.pose.position.x
+    y = data.pose.pose.position.y
+    print(x,y,euler[2])
+    if(abs(x) < 0.05 and abs(y)<0.05 and abs(euler[2])<0.09):
+        reset_odometry()
+        reset_tracking()
+        rospy.logwarn("reset_odometry")
+
+
+
+
+def listener():
+    rospy.init_node('reset_odom_node', anonymous=True)
+    rospy.wait_for_service('/zed/zed_node/reset_odometry')
+    rospy.wait_for_service('/zed/zed_node/reset_tracking')
+    rospy.Subscriber('pf/pose/odom', Odometry, subscribe_data)
+    rospy.spin()
+
+
+if __name__=="__main__":
+    listener()
