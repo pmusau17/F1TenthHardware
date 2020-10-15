b0c015c731c8099330d50f8638787cfc68d64399
Patrick Musau <pmusau13ster@gmail.com>
2020-10-15 10:45:47 -0500

    validation data is good

--- src/racecar/sys_id/bicycle_model_validation.m
+++ src/racecar/sys_id/bicycle_model_validation.m
@@ -1,6 +1,8 @@
 % validate the sysid model returned from the grey-box system identification
 % specify the parameters obtained from the sys-id in the file
 % simulate_bicycle_euler.m
-validate_experiment('csvdata_29397_2.csv');
-validate_experiment('csvdata_29397_3.csv');
-validate_experiment('csvdata_29397_4.csv');
+validate_experiment('csvdata_20774_0.csv');
+validate_experiment('csvdata_20774_1.csv');
+validate_experiment('csvdata_20774_2.csv');
+validate_experiment('csvdata_20774_3.csv');
+validate_experiment('csvdata_21100_0.csv');
