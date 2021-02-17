ba3c445401bff207bcbb5c0a6b08d6e3666d6ee4
pmusau17 <pmusau13ster@gmail.com>
2021-02-17 17:50:33 -0600

    small small changes

--- src/racecar/sys_id/validate_experiment.m
+++ src/racecar/sys_id/validate_experiment.m
@@ -48,13 +48,6 @@ legend boxoff
 
 
 subplot(2,2,[3,4]);
-% plot(x(1,:),x(2,:),'DisplayName','ground-truth,gazebo','LineWidth',2)
-% hold on;
-% plot(xe(1,:),xe(2,:),'DisplayName','sys-id model','LineWidth',2)
-% plot(x(1,1),x(2,1),'kx','DisplayName','init','LineWidth',3)
-
-
-
 plot(x(1,1),x(2,1),'*','Color', [38, 38, 38]/255,'DisplayName','init','LineWidth',2);
 hold on 
 plot(x(1,:),x(2,:),'-','Color', [157, 158, 157]/255,'DisplayName','ground-truth','LineWidth',2) ;
@@ -79,12 +72,39 @@ ax.XColor = [87, 93, 97]/255; % Red
 ax.YColor = [87, 93, 97]/255; % Blue
 set(gca,'box','off');
 
-%set(findobj(gcf,'type','axes'),'FontName','Calibri','FontSize',11,'FontWeight','Bold', 'LineWidth', 2,'layer','top');
-
+set(findobj(gcf,'type','axes'),'FontName','Calibri','FontSize',11,'FontWeight','Bold', 'LineWidth', 2,'layer','top');
 sgt =sgtitle(strcat('Validation MSE=',string(err)));
 sgt.FontSize = 20;
 figname = split(strrep(csv_filename,'csv/',''),".");
 savename = strcat("plots/",figname(1),".png");
 saveas(fig,savename);
+
+fig = figure();
+set(gcf,'color','w');
+%set(gcf, 'Position',  [100, 100, 1200, 900])
+plot(x(1,1),x(2,1),'*','Color', [38, 38, 38]/255,'DisplayName','init','LineWidth',2);
+hold on 
+plot(x(1,:),x(2,:),'-','Color', [157, 158, 157]/255,'DisplayName','ground-truth','LineWidth',2) ;
+plot(xe(1,:),xe(2,:),'--','DisplayName','prediction','Color', [70, 143, 199]/255,'LineWidth',2)
+
+hold off;
+xlabel('x (meters)') 
+ylabel('y (meters)') 
+t= title(strcat('Vehicle Position (map frame), MSE=',string(err)),'Color',[87, 93, 97]/255);
+set(t, 'horizontalAlignment', 'left')
+set(t, 'units', 'normalized')
+set(t, 'position', [0.01 1.01 0]);
+
+legend('init','ground-truth','sys-id','Location','northwest')
+legend boxoff 
+ax = gca; % Get handle to current axes.
+ax.XColor = [87, 93, 97]/255; % Red
+ax.YColor = [87, 93, 97]/255; % Blue
+set(gca,'box','off');
+set(findobj(gcf,'type','axes'),'FontName','Calibri','FontSize',11,'FontWeight','Bold', 'LineWidth', 2,'layer','top');
+
+
+
+
 end
 
